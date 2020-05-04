f8775a9658d550b6d40af57dc53427fec0235bcd
MikeWencel <wencel.michael@gmail.com>
2020-05-04 21:59:29 +0200

    Problem

--- mini_project/react-complete-guide--scripts-version/src/App.js
+++ mini_project/react-complete-guide--scripts-version/src/App.js
@@ -5,22 +5,13 @@ import Person from './Person/Person';
 
 function App() {
 
-  React.state = {
-    persons: [ 
-      {name:'Max',age: 28},
-      {name:'Josh',age: 28},
-      {name:'Mike',age: 28},
-      {name:'Katerina',age: 28}
-  
-  ]
-  }
   return (
     <div className="App">
   <h1>Hi, I'm a React App</h1>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}>Hobby: Scuba-diving</Person>
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
-        <Person name={this.state.persons[3].name} age={this.state.persons[3].age}/>
+        <Person name={"Mike"} age={Person.age}/>
+        <Person name={"Daniel"} age={Person.age}/>
+        <Person name={"Max"} age={Person.age}/>
+        <Person name={"Rafał"} age={Person.age}/>
     </div>
   );
 }
