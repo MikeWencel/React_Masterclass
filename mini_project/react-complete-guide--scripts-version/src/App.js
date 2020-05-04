dcf2da9cf9ff4280392ff6e94fb35d0b39569bc7
MikeWencel <wencel.michael@gmail.com>
2020-05-04 16:49:18 +0200

    Props and children

--- mini_project/react-complete-guide--scripts-version/src/App.js
+++ mini_project/react-complete-guide--scripts-version/src/App.js
@@ -7,9 +7,10 @@ function App() {
   return (
     <div className="App">
   <h1>Hi, I'm a React App</h1>
-        <Person/>
-        <Person/>
-        <Person/>
+        <Person name="Mike" age="33">Hobby: Scuba-diving</Person>
+        <Person name="Daniel" age="26"/>
+        <Person name="Josh" age="26"/>
+        <Person name="Katerina" age="29"/>
     </div>
   );
 }
--- mini_project/react-complete-guide--scripts-version/src/Person/Person.js
+++ mini_project/react-complete-guide--scripts-version/src/Person/Person.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
-const person = () => {
-    return <p>My names Mike and I'm {Math.floor(Math.random() * 33)} years old!</p>
+const person = (props) => {
+    return (<div>
+        <p>I'm {props.name} and I'm {Math.floor(Math.random() * 33)} years old!</p>
+            <p>{props.children}</p>
+        </div>
+)
 };
 
 export default person;
