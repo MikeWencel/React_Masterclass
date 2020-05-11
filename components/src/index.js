0b0b3e50d10c80d91419c5871653c50c6f46b456
MikeWencel <wencel.michael@gmail.com>
2020-05-11 17:48:12 +0200

    Props

--- components/src/index.js
+++ components/src/index.js
@@ -1,17 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import faker from 'faker'
 import CommentDetail from './CommentDetail';
+import faker from 'faker';
+import ApprovalCard from './ApprovalCard';
 
 const App = () => {
     return (
         <div className="ui container comments">
-            <CommentDetail />
-            <CommentDetail />
-            <CommentDetail />
-            <CommentDetail />
-            <CommentDetail />
-            <CommentDetail />
+            <ApprovalCard>
+            <CommentDetail 
+            author="Sam" 
+            timeAgo = "Today at 4:45" 
+            comment="React is really nice"
+            avatar={faker.image.avatar()}
+            />
+            </ApprovalCard>
+
+            <ApprovalCard>
+            <CommentDetail 
+            author="Jess" 
+            timeAgo = "Today at 6:45" 
+            comment="Props system are good"
+            avatar={faker.image.avatar()}
+            />
+            </ApprovalCard>
+            <ApprovalCard>
+            <CommentDetail 
+            author="Mike" 
+            timeAgo = "Today at 10:45" 
+            comment="I should learn this!"
+            avatar={faker.image.avatar()}
+            />
+            </ApprovalCard>
+           
         </div>
     );
 };
